41ba11a1b41e9bb07266071dd88ea214e47ae7a0
MarRam-1 <mariana.rmrz.96@gmail.com>
2026-04-16 16:12:01 -0600

    Api Integration

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "e439o7e5c26cc43ac0tef7fd04f093b8";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
